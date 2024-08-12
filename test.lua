5ff9797333ab6281d8480acf691c6dd2fc24f863
Par4Vox <122145600+Par4Vox@users.noreply.github.com>
2024-08-11 17:06:47 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -69,11 +69,6 @@ Hook = hookmetamethod(game, '__namecall', newcclosure(function(self, ...)
     return Hook(self, ...)
 end))
 
---// Hop Func //--
-local PlaceID = game.PlaceId
-local AllIDs = {}
-local foundAnything = ""
-local actualHour = os.date("!*t").hour
 
  local function findItem(itemName)
     local ItemsDict = {
@@ -193,27 +188,7 @@ local function farmItem(itemName, amount)
     return true
 end
 
-local function hop()
-    local Http = game:GetService("HttpService")
-    local TPS = game:GetService("TeleportService")
-    local Api = "https://games.roblox.com/v1/games/"
-
-    local _place = game.PlaceId
-    local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
-    function ListServers(cursor)
-    local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-    return Http:JSONDecode(Raw)
-    end
-
-    local Server, Next; repeat
-    local Servers = ListServers(Next)
-    Server = Servers.data[1]
-    Next = Servers.nextPageCursor
-    until Server
-
-    TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
-end
-
+local lastTick = tick()
 local function SendWebhook(msg)
     local url = getgenv().webhook
 
